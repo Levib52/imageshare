ec4eeb3af07cdabccaa305a1e35f6d56744751f3
unknown <Levi>
2020-09-29 10:48:28 -0700

    update db connection

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL=parse_url('postgres://gikbrhxwrwpxxy:18adaf44b14c7b90ea68352c37db294ac935ea623a3ac1f88b1aa8d599d8af6d@ec2-54-166-251-173.compute-1.amazonaws.com:5432/dbkqb2t9vj0hg');
 
 return [
 
@@ -15,7 +16,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL('host'),
+            'port' => $DATABASE_URL('port'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL('user'),
+            'password' => $DATABASE_URL('pass'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
